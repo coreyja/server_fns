8fd07b7a50287cca4e4bf2a64851779f90f14343
Corey Alexander <coreyja@gmail.com>
2023-11-30 11:58:01 -0500

    Return more specific types

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,42 @@
-pub mod error;
 pub mod codec;
+pub mod error;
 pub mod request;
 pub mod response;
 
+use crate::codec::{Codec, Encoding};
 use async_trait::async_trait;
 use error::ServerFnError;
 use request::Req;
 use response::Res;
-use crate::codec::{Codec, Encoding};
 
 #[async_trait]
-trait ServerFn<RequestBody, ResponseBody, Request, Response>
+trait ServerFn<RequestBody, ResponseBody, Request, Response, IntoReq, IntoResp>
 where
     Response: Res<ResponseBody> + Send + 'static,
     Request: Req<RequestBody> + Send + 'static,
     RequestBody: Send + Sync,
     ResponseBody: Send + Sync,
-    Self: Codec<RequestBody, ResponseBody, Request, Response, Self::Encoding>,
-
+    IntoReq: Req<String> + Send,
+    IntoResp: Res<String> + Send,
+    Self: Codec<RequestBody, ResponseBody, Request, Response, Self::Encoding, IntoReq, IntoResp>,
 {
     type Request;
     type Response;
     type Encoding: Encoding;
-    type Output: Codec<RequestBody, ResponseBody, Request, Response, Self::Encoding>;
+    type Output: Codec<
+        RequestBody,
+        ResponseBody,
+        Request,
+        Response,
+        Self::Encoding,
+        IntoReq,
+        IntoResp,
+    >;
 
     // the body of the fn
     fn call_fn_server(self) -> Self::Output;
 
-    async fn respond_to_request(req: Request) -> Result<Response, ServerFnError> {
+    async fn respond_to_request(req: Request) -> Result<IntoResp, ServerFnError> {
         let this = Self::from_req(req).await?;
         let output = this.call_fn_server();
         let res = output
